21a8f7af02c2bfcbc08b45a60d549b30528a30e0
nagamalleswararaoj <nmrao.mtech@gmail.com>
2019-09-07 16:09:55 +0530

    na commitment ok

---
+++ src/main/java/com/malli/example/malliexample/MalliexampleApplication.java
@@ -0,0 +1,14 @@
+package com.malli.example.malliexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalliexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalliexampleApplication.class, args);
+		System.out.println("HAI WELCOME AGAIN");
+	}
+
+}
